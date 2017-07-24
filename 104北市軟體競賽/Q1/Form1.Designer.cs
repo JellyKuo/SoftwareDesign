2330f680cd5ce7c2bf2b282b24ba637f15dd8284
Jelly Kuo <jellykuo1234@gmail.com>
2017-07-24 11:56:28 +0800

    加入北軟

--- 104北市軟體競賽/Q1/Form1.Designer.cs
+++ 104北市軟體競賽/Q1/Form1.Designer.cs
@@ -28,12 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "甲方符號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "乙方符號";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(326, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 60);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 60);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 60);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(101, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 60);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(170, 130);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 60);
+            this.button5.TabIndex = 2;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(239, 130);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 60);
+            this.button6.TabIndex = 2;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(101, 196);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 60);
+            this.button7.TabIndex = 2;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(170, 196);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 60);
+            this.button8.TabIndex = 2;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(239, 196);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(63, 60);
+            this.button9.TabIndex = 2;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(403, 287);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "自訂符號井字遊戲";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
 
--- 104北市軟體競賽/Q1/Form1.cs
+++ 104北市軟體競賽/Q1/Form1.cs
@@ -16,5 +16,37 @@ namespace Q1
         {
             InitializeComponent();
         }
+
+        bool Turn = true;
+
+        private void BtnClick(object sender, EventArgs e)
+        {
+            #region 符號判斷
+            int[] Code = new int[2];
+            Code[0] = textBox1.Text[0];
+            Code[1] = textBox2.Text[0];
+            if ((Code[0] > 90 || Code[0] < 65) && (Code[0] > 122 || Code[0] < 97))
+            {
+                MessageBox.Show("輸入的符號無效");
+                return;
+            }
+            if ((Code[1] > 90 || Code[1] < 65) && (Code[1] > 122 || Code[1] < 97))
+            {
+                MessageBox.Show("輸入的符號無效");
+                return;
+            }
+            #endregion
+            #region Set Value
+            var S = sender as Button;
+            S.Enabled = false;
+            S.Text = (Turn) ? textBox1.Text : textBox2.Text;
+            Turn = !Turn;
+            #endregion
+            #region Win
+
+            #endregion
+
+        }
+       
     }
 }
