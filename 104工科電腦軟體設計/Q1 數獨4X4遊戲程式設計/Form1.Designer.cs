385a925af2c193a4ce82e152e9f9ea52e8400e6c
Jelly Kuo <jellykuo1234@gmail.com>
2017-08-26 19:34:06 +0800

    104工科Q1 Completed basic and check, pending rewrite

---
+++ 104工科電腦軟體設計/Q1 數獨4X4遊戲程式設計/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Q1_數獨4X4遊戲程式設計
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HintBtn = new System.Windows.Forms.Button();
+            this.CheckBtn = new System.Windows.Forms.Button();
+            this.ResLab = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HintBtn
+            // 
+            this.HintBtn.Location = new System.Drawing.Point(197, 12);
+            this.HintBtn.Name = "HintBtn";
+            this.HintBtn.Size = new System.Drawing.Size(75, 23);
+            this.HintBtn.TabIndex = 1;
+            this.HintBtn.Text = "產生提示";
+            this.HintBtn.UseVisualStyleBackColor = true;
+            this.HintBtn.Click += new System.EventHandler(this.HintBtn_Click);
+            // 
+            // CheckBtn
+            // 
+            this.CheckBtn.Location = new System.Drawing.Point(197, 41);
+            this.CheckBtn.Name = "CheckBtn";
+            this.CheckBtn.Size = new System.Drawing.Size(75, 23);
+            this.CheckBtn.TabIndex = 1;
+            this.CheckBtn.Text = "Check";
+            this.CheckBtn.UseVisualStyleBackColor = true;
+            this.CheckBtn.Click += new System.EventHandler(this.CheckBtn_Click);
+            // 
+            // ResLab
+            // 
+            this.ResLab.AutoSize = true;
+            this.ResLab.Location = new System.Drawing.Point(195, 67);
+            this.ResLab.Name = "ResLab";
+            this.ResLab.Size = new System.Drawing.Size(0, 12);
+            this.ResLab.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ResLab);
+            this.Controls.Add(this.CheckBtn);
+            this.Controls.Add(this.HintBtn);
+            this.Name = "Form1";
+            this.Text = "簡易數獨";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button HintBtn;
+        private System.Windows.Forms.Button CheckBtn;
+        private System.Windows.Forms.Label ResLab;
+    }
+}
+
---
+++ 104工科電腦軟體設計/Q1 數獨4X4遊戲程式設計/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1_數獨4X4遊戲程式設計
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += (sender, e) =>
+            {
+                Pen p = new Pen(Color.Black);
+                e.Graphics.DrawLine(p, 68, 10, 68, 132);
+                e.Graphics.DrawLine(p, 5, 70, 132, 70);
+                p.Dispose();
+            };
+            GenerateUI();
+            Reset();
+        }
+
+        Label[] Lab;
+        Button[] NumBtn;
+        int SelectedBox = -1;
+
+        private void GenerateUI()
+        {
+            Lab = new Label[16];
+            int x = 0, y = -25;
+            for (int i = 0; i < 16; i++)
+            {
+                if ((i) % 4 == 0)
+                {
+                    x = 0;
+                    y += 25;
+                }
+                if ((i) % 2 == 0)
+                    x += 15;
+                if (i % 8 == 0)
+                    y += 15;
+                Lab[i] = new Label()
+                {
+                    Name = i.ToString(),
+                    AutoSize = false,
+                    Size = new Size(23, 23),
+                    Location = new Point(x, y),
+                    BorderStyle = BorderStyle.Fixed3D,
+                    TextAlign = ContentAlignment.MiddleCenter
+                };
+                x += 23;
+                Lab[i].Click += (sender, e) => SelectedBox = int.Parse((sender as Label).Name);
+                this.Controls.Add(Lab[i]);
+            }
+
+
+
+            NumBtn = new Button[4];
+            x = 10; y = 125;
+            for (int i = 0; i < 4; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    x = 10;
+                    y += 33;
+                }
+                NumBtn[i] = new Button()
+                {
+                    Text = (i + 1).ToString(),
+                    Name = (i + 1).ToString(),
+                    Size = new Size(50, 23),
+                    Location = new Point(x, y)
+                };
+                NumBtn[i].Click += (sender, e) =>
+                {
+                    try
+                    {
+                        Lab[SelectedBox].Text = (sender as Button).Name;
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        MessageBox.Show("尚未選取方格!", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                };
+                x += 60;
+                this.Controls.Add(NumBtn[i]);
+            }
+        }
+
+        private void Reset()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Lab[i].Text = "1";
+                        break;
+                    case 5:
+                        Lab[i].Text = "2";
+                        break;
+                    case 10:
+                        Lab[i].Text = "3";
+                        break;
+                    case 15:
+                        Lab[i].Text = "4";
+                        break;
+                    default:
+                        Lab[i].Text = "";
+                        break;
+                }
+                SelectedBox = -1;
+            }
+        }
+
+        private void CheckBtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int Sum = 0;
+                for (int j = 0; j < 4; j++)
+                    Sum += int.Parse(Lab[(j * 4) + i].Text);
+                if (Sum != 10)
+                {
+                    ResLab.Text = "錯誤";
+                    Console.WriteLine("Wrong on column {0}, sum: {1}", i, Sum);
+                    return;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int Sum = 0;
+                for (int j = 0; j < 4; j++)
+                    Sum += int.Parse(Lab[j + (i * 4)].Text);
+                if (Sum != 10)
+                {
+                    ResLab.Text = "錯誤";
+                    Console.WriteLine("Wrong on row {0}, sum: {1}", i, Sum);
+                    return;
+                }
+            }
+
+            int[] Header = new int[] { 0, 2, 8, 10 };
+            for (int i = 0; i < 4; i++)
+            {
+                int Sum = 0;
+                Sum += int.Parse(Lab[Header[i]].Text);
+                Sum += int.Parse(Lab[Header[i] + 1].Text);
+                Sum += int.Parse(Lab[Header[i] + 4].Text);
+                Sum += int.Parse(Lab[Header[i] + 5].Text);
+                if (Sum != 10)
+                {
+                    ResLab.Text = "錯誤";
+                    Console.WriteLine("Wrong on block {0}, sum: {1}", i, Sum);
+                    return;
+                }
+            }
+        }
+
+        private void HintBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
