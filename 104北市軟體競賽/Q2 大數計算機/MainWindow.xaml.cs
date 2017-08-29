4f0455306e94d2d9d38b6f837f6cb6eda0784bc3
Jelly Kuo <jellykuo1234@gmail.com>
2017-08-29 22:35:23 +0800

    Start work on 104北Q2BigCalc

--- 104北市軟體競賽/Q2 大數計算機/MainWindow.xaml.cs
+++ 104北市軟體競賽/Q2 大數計算機/MainWindow.xaml.cs
@@ -24,5 +24,56 @@ namespace Q2_大數計算機
         {
             InitializeComponent();
         }
+
+        public const int Len = 200;
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BoxA.Text = "";
+            BoxB.Text = "";
+            ResBox.Text = "";
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            var Btn = sender as Button;
+            var A = new char[Len];
+            var B = new char[Len];
+            for(int i = 0; i < BoxA.Text.Length; i++)
+            {
+                A[BoxA.Text.Length-1 - i] = BoxA.Text[i];
+            }
+            for (int i = 0; i < BoxB.Text.Length; i++)
+            {
+                B[BoxB.Text.Length - 1 - i] = BoxB.Text[i];
+            }
+            char[] Result = null ;
+            switch (Btn.Name)
+            {
+                case "PlusBtn":
+                    Result = Plus(A, B);
+                    break;
+                case "SubBtn":
+                    break;
+                case "MutBtn":
+                    break;
+            }
+            Array.Reverse(Result);
+            ResBox.Text = new string(Result);
+        }
+
+        private char[] Plus(char[] A,char[] B)
+        {
+            var Result = new char[Len];
+            for(int i = 0; i< Len; i++)
+            {
+                if (A[i] == 0)
+                    A[i] = (char)48;
+                if (B[i] == 0)
+                    B[i] = (char)48;
+                Result[i] = (char)(A[i] + B[i] - 48);
+            }
+            return Result;
+        }
     }
 }
