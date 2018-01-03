ddb61a9ed0def14da4b267db4d65f25ec396b4ce
Jelly Kuo <jellykuo1234@gmail.com>
2018-01-03 20:53:01 +0800

    Added files for 46全國技能競賽北區

---
+++ 47全國技能_北區/02 SoftwareDesign/Data/CO01/COD01/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO; 
+
+namespace COD01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 建立XmlDocment物件xmldoc
+            // TODO
+            XmlNode xmlNode;
+            // 使用FileStream讀取XML文件books.xml
+            // TODO
+            // 將XML文件載入XmlDocment物件xmldoc
+            // TODO            
+            treeView1.Nodes.Clear();
+            // 新增TreeView控制項的根元素，即library
+            treeView1.Nodes.Add(// TODO);
+            // 取得XML文件的所有book子元素
+            xmlNode = // TODO
+            TreeNode treeNode;
+            treeNode = treeView1.Nodes[0];
+            // 呼叫遞迴函數來新增節點
+            AddNode(xmlNode, treeNode);
+        }
+
+        private void AddNode(XmlNode xmlNode, TreeNode treeNode)
+        {
+            XmlNode xNode;
+            TreeNode tNode;
+            XmlNodeList nodeList;
+            int i = 0;
+            if (xmlNode.HasChildNodes)  // 是否有子節點
+            {
+                // 取得子節點清單的XmlNodeList物件
+                nodeList = xmlNode.ChildNodes;
+                // 一一取出XmlNodeList物件的節點
+                for (i = 0; i <= nodeList.Count - 1; i++)
+                {
+                    // 取得每一個子節點XmlNode物件
+                    xNode = // TODO
+                    // 新增TreeView控制項的節點名稱
+                    treeNode.Nodes.Add(new TreeNode(// TODO));
+                    // 取得TreeNode物件 
+                    tNode = treeNode.Nodes[i];
+                    AddNode(xNode, tNode);  // 呼叫遞迴函數
+                }
+            }
+            else
+            {
+                // 沒有子節點，取得節點的文字內容
+                treeNode.Text = // TODO
+            }
+        }
+    }
+}
---
+++ 47全國技能_北區/02 SoftwareDesign/Data/CO02/COD04/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace COD04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDataList = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.cmdOpenRead = new System.Windows.Forms.Button();
+            this.cmdBrowse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDataList
+            // 
+            this.dgvDataList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataList.Location = new System.Drawing.Point(38, 71);
+            this.dgvDataList.Name = "dgvDataList";
+            this.dgvDataList.RowTemplate.Height = 24;
+            this.dgvDataList.Size = new System.Drawing.Size(803, 387);
+            this.dgvDataList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "資料來源：";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(107, 25);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(605, 22);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // cmdOpenRead
+            // 
+            this.cmdOpenRead.Location = new System.Drawing.Point(749, 25);
+            this.cmdOpenRead.Name = "cmdOpenRead";
+            this.cmdOpenRead.Size = new System.Drawing.Size(92, 23);
+            this.cmdOpenRead.TabIndex = 3;
+            this.cmdOpenRead.Text = "讀取";
+            this.cmdOpenRead.UseVisualStyleBackColor = true;
+            this.cmdOpenRead.Click += new System.EventHandler(this.cmdOpenRead_Click);
+            // 
+            // cmdBrowse
+            // 
+            this.cmdBrowse.Location = new System.Drawing.Point(718, 25);
+            this.cmdBrowse.Name = "cmdBrowse";
+            this.cmdBrowse.Size = new System.Drawing.Size(25, 23);
+            this.cmdBrowse.TabIndex = 4;
+            this.cmdBrowse.Text = "...";
+            this.cmdBrowse.UseVisualStyleBackColor = true;
+            this.cmdBrowse.Click += new System.EventHandler(this.cmdBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 489);
+            this.Controls.Add(this.cmdBrowse);
+            this.Controls.Add(this.cmdOpenRead);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDataList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "高雄市公共腳踏車租賃站";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDataList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button cmdOpenRead;
+        private System.Windows.Forms.Button cmdBrowse;
+    }
+}
+
